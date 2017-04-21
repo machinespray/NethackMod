497af899442631a0b6059b59e3fb6d01326805bc
machinespray <machinespray1@gmail.com>
2017-04-21 14:45:28 -0400

    Updated mcmod.info, fixed protection ring.

--- src/main/java/com/machinespray/ROYAL/Main.java
+++ src/main/java/com/machinespray/ROYAL/Main.java
@@ -29,7 +29,7 @@ public class Main
 	@CapabilityInject(IKnowledgeHandler.class)
     public static final Capability<IKnowledgeHandler> CAPABILITY_KNOWLEDGE = null;
     public static final String MODID = "royal";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     public static final Random random = new Random();
     @SidedProxy(modId=MODID,clientSide="com.machinespray.ROYAL.proxy.ClientProxy",serverSide="com.machinespray.ROYAL.proxy.CommonProxy")
     public static CommonProxy proxy;
