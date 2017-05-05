58268a560f8a37689587a63896045f8f0719f45c
machinespray <machinespray1@gmail.com>
2017-05-05 16:24:55 -0400

    v0.24 changes

--- src/main/java/com/machinespray/ROYAL/Main.java
+++ src/main/java/com/machinespray/ROYAL/Main.java
@@ -29,7 +29,7 @@ public class Main
 	@CapabilityInject(IKnowledgeHandler.class)
     public static final Capability<IKnowledgeHandler> CAPABILITY_KNOWLEDGE = null;
     public static final String MODID = "royal";
-    public static final String VERSION = "0.23";
+    public static final String VERSION = "0.24";
     public static final Random random = new Random();
     @SidedProxy(modId=MODID,clientSide="com.machinespray.ROYAL.proxy.ClientProxy",serverSide="com.machinespray.ROYAL.proxy.CommonProxy")
     public static CommonProxy proxy;
